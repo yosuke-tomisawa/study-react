36113d412fb3786a7d3f4f2aa9eb7b65a7825498
yosuke-tomisawa <bhav.urs@gmail.com>
2024-10-06 23:47:07 +0900

    ✨ ヘッダー追加

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import classes from "./Header.module.css";
+
+export function Header() {
+  return (
+    <header className={classes.header}>
+      <Link className={classes.anchor} href="/">
+        Index
+      </Link>
+      <Link className={classes.anchor} href="/about">
+        About
+      </Link>
+    </header>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
 import { Footer } from "../../components/Footer";
 import { Links } from "../../components/Links";
+import { Header } from "../../components/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,7 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
+          <Header />
           <h1>Index Page</h1>
           <Links onClick={() => alert("トップページです")}>
             <ol>
